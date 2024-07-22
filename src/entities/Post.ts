e8a19918a46e763e243ce2547e93d6d2589307db
Reeve Fernandes <ferna312@msu.edu>
2024-07-22 14:46:59 -0400

    Post CRUD done

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType, Int } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
+	@Field(() => Int)
 	@PrimaryKey()
 	id!: number;
 
+	@Field(() => String)
 	@Property({ default: "now()" })
 	createdAt = new Date();
 
+	@Field(() => String)
 	@Property({ onUpdate: () => new Date() })
 	updatedAt = new Date();
 
+	@Field()
 	@Property({ type: "text" })
 	title!: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,43 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import mikroOrmConfig from "./mikro-orm.config";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
 	const orm = await MikroORM.init(mikroOrmConfig);
 	await orm.getMigrator().up();
 
-	const post = orm.em.create(Post, {
-		title: "Hello World",
-		createdAt: new Date(),
-		updatedAt: new Date()
-	});
-	await orm.em.persistAndFlush(post);
+	const app = express();
 
-	const posts = await orm.em.find(Post, {});
-	console.log(posts);
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver, PostResolver],
+			validate: false
+		}),
+		context: () => ({ em: orm.em })
+	});
+
+	await apolloServer.start();
+	apolloServer.applyMiddleware({ app } as { app: any });
+
+	app.listen(4000, () => {
+		console.log("Server started on localhost:4000");
+	});
+
+	// const post = orm.em.create(Post, {
+	// 	title: "Hello World",
+	// 	createdAt: new Date(),
+	// 	updatedAt: new Date()
+	// });
+	// await orm.em.persistAndFlush(post);
+
+	// const posts = await orm.em.find(Post, {});
+	// console.log(posts);
 };
 
 main().catch((err) => {
